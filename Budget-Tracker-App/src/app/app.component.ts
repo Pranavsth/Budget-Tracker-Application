d8b8dabbe2331ab60cc53ca7c64a0fb51def2e25
pranav <pranav.shrestha@bajratechnologies.com>
2024-01-12 16:59:13 +0545

    Registration and Login component created

---
+++ Budget-Tracker-App/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Budget-Tracker-App';
+}
